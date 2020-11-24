a58e3045833186bf3d12beeed1e1ea4419d398f1
Dylan Stone-Miller <stonemiller.dylan@gmail.com>
2020-11-24 00:18:35 -0500

    initital commit

--- App.js
+++ App.js
@@ -2,7 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
+import { withAuthenticator } from 'aws-amplify-react-native';
+
+function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -10,6 +15,8 @@ export default function App() {
     </View>
   );
 }
+
+export default withAuthenticator(App,true)
 
 const styles = StyleSheet.create({
   container: {
